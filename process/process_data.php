f43cbd69ed3d37967a215325e7918cc911527238
beckpalmx77 <beckpalmx@gmail.com>
2022-04-29 21:37:11 +0700

    upa

--- process/process_data.php
+++ process/process_data.php
@@ -2,6 +2,7 @@
 
 include("../config/connect_db.php");
 
+$str_file = "";
 
 $sql_get = " SELECT PGROUP,BRN_CODE,BRN_NAME,DI_MONTH,DI_YEAR
 ,sum(CAST(TRD_QTY AS DECIMAL(10,2))) as TRD_QTY
@@ -17,6 +18,9 @@ $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 foreach ($results as $result) {
 
     //echo $result['BRN_NAME'] . " | " . $result['DI_MONTH'] . " | " . $result['DI_YEAR'] . " | " . $result['TRD_G_KEYIN'] . "<br>";
+
+    $F_TRD_QTY = "";
+    $F_TRD_G_KEYIN = "";
 
     switch ($result['DI_MONTH']) {
         case "1":
@@ -69,6 +73,18 @@ foreach ($results as $result) {
             break;
     }
 
+    $TRD_QTY = $result['TRD_QTY'];
+    $TRD_G_KEYIN = $result['TRD_G_KEYIN'];
+
+    $str_file .= $result['DI_MONTH'] . " | F_TRD_QTY  = " . $F_TRD_QTY . "| F_TRD_G_KEYIN = " . $F_TRD_G_KEYIN
+        . " | TRD_QTY  = " . $result['TRD_QTY'] . "| TRD_G_KEYIN = " . $result['TRD_G_KEYIN']
+        . "\n\r";
+
+    $myfile = fopen("param-1.txt", "w") or die("Unable to open file!");
+    fwrite($myfile, $str_file);
+    fclose($myfile);
+
+
     $sql_find = " SELECT * 
 FROM ims_product_brand_monthly 
 WHERE PGROUP = '" . $result['PGROUP'] . "'
@@ -79,32 +95,32 @@ AND DI_YEAR = '" . $result['DI_YEAR'] . "'
     echo $sql_find . "<br>";
 
     $nRows = $conn->query($sql_find)->fetchColumn();
-    if ($nRows > 0) {
-        $sql_update = "UPDATE ims_product_brand_monthly SET "
-            . $F_TRD_QTY . "=:TRD_QTY, " . $F_TRD_QTY . "=:TRD_G_KEYIN             
-            WHERE PGROUP = :PGROUP AND BRN_CODE = :BRN_CODE AND BRN_NAME = :BRN_NAME AND DI_YEAR = :DI_YEAR ";
-        $query_update = $conn->prepare($sql_update);
-        $query_update->bindParam(':TRD_QTY', $result['TRD_QTY'], PDO::PARAM_STR);
-        $query_update->bindParam(':TRD_G_KEYIN', $result['TRD_G_KEYIN'], PDO::PARAM_STR);
-        $query_update->bindParam(':PGROUP', $result['PGROUP'], PDO::PARAM_STR);
-        $query_update->bindParam(':BRN_CODE', $result['BRN_CODE'], PDO::PARAM_STR);
-        $query_update->bindParam(':BRN_NAME', $result['BRN_NAME'], PDO::PARAM_STR);
-        $query_update->bindParam(':DI_YEAR', $result['DI_YEAR'], PDO::PARAM_STR);
-        echo " sql_update = " . $sql_update . "<br>";
-        $query_update->execute();
-    } else {
-        $sql_insert = "INSERT INTO ims_product_brand_monthly(". $F_TRD_QTY . "," . $F_TRD_G_KEYIN . ",PGROUP,BRN_CODE,BRN_NAME,DI_YEAR) 
+    if ($nRows <= 0) {
+        $sql_insert = "INSERT INTO ims_product_brand_monthly(" . $F_TRD_QTY . "," . $F_TRD_G_KEYIN . ",PGROUP,BRN_CODE,BRN_NAME,DI_YEAR) 
         VALUES (:TRD_QTY,:TRD_G_KEYIN,:PGROUP,:BRN_CODE,:BRN_NAME,:DI_YEAR)";
         $query_inset = $conn->prepare($sql_insert);
-        $query_inset->bindParam(':TRD_QTY', $result['TRD_QTY'], PDO::PARAM_STR);
-        $query_inset->bindParam(':TRD_G_KEYIN', $result['TRD_G_KEYIN'], PDO::PARAM_STR);
+        $query_inset->bindParam(':TRD_QTY', $TRD_QTY, PDO::PARAM_STR);
+        $query_inset->bindParam(':TRD_G_KEYIN', $TRD_G_KEYIN, PDO::PARAM_STR);
         $query_inset->bindParam(':PGROUP', $result['PGROUP'], PDO::PARAM_STR);
         $query_inset->bindParam(':BRN_CODE', $result['BRN_CODE'], PDO::PARAM_STR);
         $query_inset->bindParam(':BRN_NAME', $result['BRN_NAME'], PDO::PARAM_STR);
         $query_inset->bindParam(':DI_YEAR', $result['DI_YEAR'], PDO::PARAM_STR);
-        echo " sql_insert = " . $sql_insert . "<br>";
+        //echo " sql_insert = " . $sql_insert . "<br>";
         $query_inset->execute();
-
+    } else {
+        $sql_update = "UPDATE ims_product_brand_monthly SET "
+            . $F_TRD_QTY . "=:TRD_QTY, " . $F_TRD_QTY . "=:TRD_G_KEYIN             
+            WHERE PGROUP = :PGROUP AND BRN_CODE = :BRN_CODE AND BRN_NAME = :BRN_NAME AND DI_YEAR = :DI_YEAR ";
+        $query_update = $conn->prepare($sql_update);
+        $query_update->bindParam(':TRD_QTY', $TRD_QTY, PDO::PARAM_STR);
+        $query_update->bindParam(':TRD_G_KEYIN', $TRD_G_KEYIN, PDO::PARAM_STR);
+        $query_update->bindParam(':PGROUP', $result['PGROUP'], PDO::PARAM_STR);
+        $query_update->bindParam(':BRN_CODE', $result['BRN_CODE'], PDO::PARAM_STR);
+        $query_update->bindParam(':BRN_NAME', $result['BRN_NAME'], PDO::PARAM_STR);
+        $query_update->bindParam(':DI_YEAR', $result['DI_YEAR'], PDO::PARAM_STR);
+        //echo " sql_update = " . $sql_update . "<br>";
+        $query_update->execute();
     }
 
 }
+
