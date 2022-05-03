c22dae55ccdfdbec7ffd18fbbc0d022236bb6370
beckpalmx77 <beckpalmx@gmail.com>
2022-05-03 21:43:27 +0700

    Update framework_util.js

--- js/MyFrameWork/framework_util.js
+++ js/MyFrameWork/framework_util.js
@@ -13,7 +13,8 @@ function isEmpty(obj) {
 
 
 function ValidateEmail(inputText) {
-    let mail_format = /^[a-zA-Z0-9\.\_]+\@@{1}[a-zA-Z0-9]+\.\w{2,4}$/;
+    //let mail_format = /^[a-zA-Z0-9\.\_]+\@@{1}[a-zA-Z0-9]+\.\w{2,4}$/;
+    let mail_format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!mail_format.test(inputText)) {
         return false;
     } else {
