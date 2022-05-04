a7f2444e42776b44ac95ae2ca7c677c4b17c286c
beckpalmx77 <beckpalmx@gmail.com>
2022-05-04 09:48:12 +0700

    Update display_chart_cockpit-4.php

--- display_chart_cockpit-4.php
+++ display_chart_cockpit-4.php
@@ -97,7 +97,7 @@ if (strlen($_SESSION['alogin']) == "") {
                                                                                 <div class="row">
                                                                                     <div class="col-sm-12">
                                                                                         <button type="button" id="BtnSale" name="BtnSale" class="btn btn-primary mb-3">แสดง
-                                                                                            Chart ยอดขายยางแต่ละยี่ห้อ
+                                                                                            Chart ยอดขายยางแต่ละยี่ห้อ (Cockpit)
                                                                                         </button>
                                                                                     </div>
                                                                                 </div>
