45a56e88c1844efdd15716cad146d63b10a60987
beckpalmx77 <beckpalmx@gmail.com>
2023-04-17 09:39:25 +0700

    Update import_data_ardetail.php

--- cronjob/import_data_ardetail.php
+++ cronjob/import_data_ardetail.php
@@ -6,7 +6,18 @@ error_reporting(~0);
 include ("../config/connect_sqlserver.php");
 include ("../config/connect_db.php");
 
-$sql_sqlsvr = "select * from ardetail ";
+
+$sql_keymax = " select ARD_KEY from ardetail order by ARD_KEY desc  limit 1  ";
+$statement = $conn->query($sql_keymax);
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($results as $result) {
+
+    $ARD_KEY_LAST = $result['ARD_KEY'];
+
+}
+
+
+$sql_sqlsvr = "select * from ardetail where ARD_KEY >= " . $ARD_KEY_LAST;
 
 //$myfile = fopen("qry_file1.txt", "w") or die("Unable to open file!");
 //fwrite($myfile, $sql_sqlsvr);
