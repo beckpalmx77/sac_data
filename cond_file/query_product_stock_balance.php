16ca85aa3cb50a98ccfda4c94b8857ede44736b1
beckpalmx77 <beckpalmx@gmail.com>
2023-04-20 12:59:30 +0700

    update

--- cond_file/query_product_stock_balance.php
+++ cond_file/query_product_stock_balance.php
@@ -28,9 +28,8 @@ FROM
   JOIN DOCINFO ON SKUMOVE.SKM_DI=DOCINFO.DI_KEY
   JOIN DOCTYPE ON  DOCINFO.DI_DT=DOCTYPE.DT_KEY ";
 
-$sql_cond_stock = "";
 
-// $sql_cond_stock = " WHERE
+//$sql_cond_stock = " WHERE
 // SKUMASTER.SKU_STOCK <> 0 ";
 
 $sql_group_stock = " GROUP BY
