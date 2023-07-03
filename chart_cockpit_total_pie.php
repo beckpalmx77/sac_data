9153bd4f232d28296ce63f3b64334542cca21e86
beckpalmx77 <beckpalmx@gmail.com>
2023-07-03 13:21:17 +0700

    up

--- chart_cockpit_total_pie.php
+++ chart_cockpit_total_pie.php
@@ -109,7 +109,6 @@ include("display_data_cockpit_detail.php");
                 "#00aba9",
                 "#f81b61",
                 "#fcae13"
-
             ];
 
             $.post("engine/chart_data_pie_daily.php", {doc_date: doc_date ,branch: branch }, function (data) {
@@ -216,8 +215,6 @@ include("display_data_cockpit_detail.php");
     }
 
 </script>
-
-
 
 </body>
 </html>
--- export_process/export_process_data_sale_total_cp.php
+++ export_process/export_process_data_sale_total_cp.php
@@ -20,7 +20,7 @@ $filename = $branch . "-" . "Total_Data_Sale_CP" . "-" . date('m/d/Y H:i:s', tim
 @header('Content-Encoding: UTF-8');
 @header("Content-Disposition: attachment; filename=" . $filename);
 
-$select_query_daily = "  SELECT DI_REF,DT_DOCCODE,AR_NAME ,SUM(TRD_G_KEYIN) AS SUM_TOTAL,BRANCH,DI_MONTH_NAME,DI_YEAR FROM ims_product_sale_cockpit ";
+$select_query_daily = "  SELECT DI_REF,DT_DOCCODE,AR_NAME,PGROUP,SUM(TRD_G_KEYIN) AS SUM_TOTAL,BRANCH,DI_MONTH_NAME,DI_YEAR FROM ims_product_sale_cockpit ";
 
 if ($myCheckValue === 'Y') {
     $select_where_daily = " WHERE DI_YEAR = " . $year;
@@ -28,8 +28,8 @@ if ($myCheckValue === 'Y') {
     $select_where_daily = " WHERE DI_MONTH = " . $month . " AND DI_YEAR = " . $year;
 }
 
-$select_group_order = " GROUP BY DI_REF 
-ORDER BY AR_NAME,BRANCH ";
+$select_group_order = " GROUP BY DI_REF,PGROUP 
+ORDER BY AR_NAME,DI_REF,PGROUP,BRANCH ";
 
 switch ($branch) {
     case "CP-340":
@@ -52,11 +52,12 @@ switch ($branch) {
 
 $String_Sql = $select_query_daily . $select_where_daily . $query_daily_cond_ext . $select_group_order;
 
-//$my_file = fopen("D-sac_str_return.txt", "w") or die("Unable to open file!");
-//fwrite($my_file,$String_Sql);
-//fclose($my_file);
+/* $my_file = fopen("PGROUP.txt", "w") or die("Unable to open file!");
+fwrite($my_file,$String_Sql);
+fclose($my_file);
+*/
 
-$data = "เลขที่เอกสาร,ชื่อลูกค้า,มูลค่ารวม,สาขา,เดือน,ปี\n";
+$data = "เลขที่เอกสาร,ชื่อลูกค้า,ประเภท,มูลค่ารวม,สาขา,เดือน,ปี\n";
 
 $query = $conn->prepare($String_Sql);
 $query->execute();
@@ -65,8 +66,24 @@ $results = $query->fetchAll(PDO::FETCH_OBJ);
 if ($query->rowCount() >= 1) {
     foreach ($results as $result) {
 
+        switch ($result->PGROUP) {
+            case "P1":
+                $PGROUP = "ยาง";
+                break;
+            case "P2":
+                $PGROUP = "อะไหล่";
+                break;
+            case "P3":
+                $PGROUP = "ค่าแรง-ค่าบริการ";
+                break;
+            case "P4":
+                $PGROUP = "อื่นๆ";
+                break;
+        }
+
         $data .= " " . $result->DI_REF . ",";
         $data .= " " . $result->AR_NAME . ",";
+        $data .= " " . $PGROUP . ",";
         $data .= " " . $result->SUM_TOTAL . ",";
         $data .= " " . $result->BRANCH . ",";
         $data .= " " . $result->DI_MONTH_NAME . ",";
