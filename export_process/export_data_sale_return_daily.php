41e63d4540525436e214e9b6eb3668acc4c84142
beckpalmx77 <beckpalmx@gmail.com>
2024-09-27 09:05:37 +0700

    Update export_data_sale_return_daily.php

--- export_process/export_data_sale_return_daily.php
+++ export_process/export_data_sale_return_daily.php
@@ -23,7 +23,7 @@ $String_Sql = $select_query_daily . $select_query_daily_cond . " AND DI_DATE BET
 //fwrite($my_file, $String_Sql);
 //fclose($my_file);
 
-$data = "DI_DATE,,,AR_CODE,SKU_CODE,SKU_NAME,BRN_NAME,BRN_CODE,DI_REF,AR_NAME,SLMN_NAME,,TRD_QTY,TRD_U_PRC,TRD_DSC_KEYINV,TRD_B_SELL,TRD_B_VAT,TRD_G_KEYIN,,,WL_CODE,TRD_SEQ\n";
+$data = "DI_DATE,,,AR_CODE,SKU_CODE,SKU_NAME,BRN_NAME,BRN_CODE,DI_REF,AR_NAME,SLMN_NAME,,TRD_QTY,TRD_U_PRC,TRD_DSC_KEYINV,TRD_B_SELL,TRD_B_VAT,TRD_G_KEYIN,,,WL_CODE\n";
 
 $query = $conn_sqlsvr->prepare($String_Sql);
 $query->execute();
@@ -92,8 +92,7 @@ if(strpos($row['DT_DOCCODE'], $DT_DOCCODE_MINUS) !== false){
 
     $data .= " " . ",";
     $data .= " " . ",";
-    $data .= str_replace(",", "^", $row['WL_CODE']) . ",";
-    $data .= str_replace(",", "^", $row['TRD_SEQ']) . "\n";
+    $data .= str_replace(",", "^", $row['WL_CODE']) . "\n";
 
 
 }
