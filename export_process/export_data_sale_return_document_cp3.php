dc4dbc6e81bf76b735973d7bdb19143fdd535281
beckpalmx@gmail.com <beckpalmx@gmail.com>
2025-01-30 10:15:11 +0700

    Update export_data_sale_return_document_cp3.php

--- export_process/export_data_sale_return_document_cp3.php
+++ export_process/export_data_sale_return_document_cp3.php
@@ -33,7 +33,7 @@ if ($query->rowCount() == 0) {
     die("❌ ไม่พบข้อมูลในฐานข้อมูล");
 }
 
-$data = "ลำดับที่,วัน,เดือน,ปี,เลขที่เอกสาร,ชื่อลูกค้า,หมายเลขโทรศัพท์,ทะเบียนรถ,ยี่ห้อรถ/รุ่น,เลขไมล์,รหัสสินค้า,ชื่อสินค้า,จำนวน,จำนวนเงิน\n";
+$data = "ลำดับที่,วัน,เดือน,ปี,เลขที่เอกสาร,รหัสลูกค้า,ชื่อลูกค้า,หมายเลขโทรศัพท์,ทะเบียนรถ,ยี่ห้อรถ/รุ่น,เลขไมล์,รหัสสินค้า,ชื่อสินค้า,จำนวน,จำนวนเงิน\n";
 
 $line = 0;
 while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
@@ -57,6 +57,20 @@ while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
         $addb_phone = $result_sqlsvr_cust['ADDB_PHONE'] === null ? "" : $result_sqlsvr_cust['ADDB_PHONE'];
     }
 
+    $sql_cust_string2 = "
+    SELECT TOP 1 ARFILE.AR_CODE
+    FROM ARFILE        
+    WHERE ARFILE.AR_NAME = :company;
+";
+    $statement_cust_sqlsvr2 = $conn_sqlsvr->prepare($sql_cust_string2);
+    $statement_cust_sqlsvr2->bindValue(':company',  $row['ADDB_COMPANY'] , PDO::PARAM_STR);
+    $statement_cust_sqlsvr2->execute();
+
+    $AR_CODE = "";
+    if ($result_sqlsvr_cust2 = $statement_cust_sqlsvr2->fetch(PDO::FETCH_ASSOC)) {
+        $AR_CODE = $result_sqlsvr_cust2['AR_CODE'] === null ? "" : $result_sqlsvr_cust2['AR_CODE'];
+    }
+
     $ADDB_COMPANY = $row['ADDB_COMPANY'] === null ? "" : $row['ADDB_COMPANY'];
     $addb_phone = $result_sqlsvr_cust['ADDB_PHONE'] === null ? "" : $result_sqlsvr_cust['ADDB_PHONE'];
     $ADDB_ADDB_1 = $row['ADDB_ADDB_1'] === null ? "" : $row['ADDB_ADDB_1'];
@@ -66,6 +80,7 @@ while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
 
     $data .= "$line,{$row['DI_DAY']},$month_name,$year,";
     $data .= str_replace(",", " ", $row['DI_REF']) . ",";
+    $data .= str_replace(",", " ", $AR_CODE) . ",";
     $data .= str_replace(",", " ", $ADDB_COMPANY) . ",";
     $data .= str_replace(",", " ", $addb_phone) . ",";
     $data .= str_replace(",", " ", $ADDB_BRANCH) . ",";
